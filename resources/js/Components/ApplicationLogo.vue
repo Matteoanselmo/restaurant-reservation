a5bc24f702bdd02d1013c6d71b95aa62f105c8b2
MATEO ANSELMO <matteo.anselmo96@gmail.com>
2023-10-10 11:56:32 +0200

    add application logo in prenotation table

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -2,7 +2,7 @@
     <div class="d-flex flex-column">
         <!-- <h1 class="text-uppercase">villa albertina</h1>
         <p class="text-center">logo</p> -->
-        <img src="/images/logo/logo.png" class="w-50 h-50" alt="">
+        <img src="/images/logo/logo.png" class="h-75" alt="">
     </div>
 </template>
 
