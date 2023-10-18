cbac19c0ee8fa74e768432bfabe55d141808204f
MATEO ANSELMO <matteo.anselmo96@gmail.com>
2023-10-18 14:34:59 +0200

    add input-file in create-date with custom resize

--- resources/js/Pages/Dashboard/Date/Create.vue
+++ resources/js/Pages/Dashboard/Date/Create.vue
@@ -1,48 +1,57 @@
 <template>
-    <div class="full-screen d-flex flex-column px-3 justify-content-start mt-3">
-        <h1 class="text-center mb-4 fw-bold">Spettacolo di {{new Date(data).toLocaleDateString('it-IT', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }) }}</h1>
+    <div class="container-fluid d-flex flex-column px-3 justify-content-start mt-3">
+        <h1 class="text-center mb-4 fw-bold animated animate__fadeInDown text-capitalize">Spettacolo {{new Date(data).toLocaleDateString('it-IT', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }) }}</h1>
         <form @submit.prevent="createReservationDate">
-            <div class="row align-items-center justify-content-center">
-                <div class="col-12 col-md-5 mb-3" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="Semplice ed efficace">
-                    <label class="mb-1 fs-4" for="titolo">Titolo:</label>
-                    <input type="text" id="titolo" class="form-control" v-model="formData.titolo" required>
+            <div class="row gy-4 align-items-center justify-content-center">
+                <div class="col-12 col-md-5 d-flex flex-column align-items-start justify-content-start mb-4 h-100 animated animate__fadeInBottomLeft">
+                    <div class="mb-3 w-100">
+                        <label class="mb-1 fs-4" for="titolo">Titolo: *</label>
+                        <div class="d-flex align-items-center justify-content-center">
+                            <input type="text" id="titolo" class="form-control me-3" v-model.trim="formData.titolo" required>
+                        </div>
+                    </div>
+                    <div class="mb-3 w-100">
+                        <label class="mb-1 fs-4" for="showType">Show Type: *</label>
+                        <div class="d-flex align-items-center justify-content-center">
+                            <select id="showType" class="form-control me-3" v-model="formData.show_type_id" required>
+                                <option class="text-uppercase" v-for="(showType, x) in showTypes" :key="x" :value="showType.id">{{ showType.nome }}</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="mb-3 w-100">
+                        <label class="mb-1 fs-4" for="mealType">Ora Spettacolo: *</label>
+                        <div class="d-flex align-items-center justify-content-center">
+                            <select id="mealType" class="form-control me-3" v-model="formData.pranzo_cena" required>
+                                <option value="pranzo">Pranzo</option>
+                                <option value="cena">Cena</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-12 col-md-5 mb-3" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="Aggiungi una descrizione precisa e non troppo lunga">
+                <div class="col-12 col-md-5 d-flex flex-column align-items-start justify-content-start mb-4 h-100 border-start border-2 animated animate__fadeInBottomRight">
                     <label class="mb-1 fs-4" for="descrizione">Descrizione:</label>
-                    <textarea id="descrizione" class="form-control" v-model="formData.descrizione" required></textarea>
-                </div>
-                <!-- <div class="col-12 col-md-5 mb-3">
-                    <label class="mb-1 fs-4" for="maxSeats">Posti Massimi:</label>
-                    <input type="number" id="maxSeats" class="form-control" v-model="formData.posti_disponibili" min="1" max="8" required>
-                </div> -->
-                <div class="col-12 col-md-5 mb-3">
-                    <label class="mb-1 fs-4" for="showType">Show Type:</label>
-                    <select id="showType" class="form-control" v-model="formData.show_type_id" required>
-                        <option class="text-uppercase" v-for="(showType, x) in showTypes" :key="x" :value="showType.id">{{ showType.nome }}</option>
-                    </select>
-                </div>
-                <div class="col-12 col-md-5 mb-5">
-                    <label class="mb-1 fs-4" for="mealType">Ora Spettacolo:</label>
-                    <select id="mealType" class="form-control" v-model="formData.pranzo_cena" required>
-                        <option value="pranzo">Pranzo</option>
-                        <option value="cena">Cena</option>
-                    </select>
-                </div>
-                <div class="col-12 col-md-5 mb-5" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="Formatta il prezzo inserendo la visrgola da solo">
+                    <textarea id="descrizione" class="form-control" v-model.trim="formData.descrizione" rows="10" required></textarea>
                     <label for="prezzo" class="mb-1 fs-4">
                     Prezzo
                     </label>
                     <input
                         type="text"
                         id="prezzo"
-                        class="form-control"
+                        class="form-control mb-4"
                         v-model.number="formData.prezzo"
                         step=0.01
                         @input="formatCurrency"
                     >
+                    <input type="file" class="form-control mb-3" @change="onFileChange" multiple>
+                    <!-- Anteprime delle immagini -->
+                    <div v-if="imagePreviews.length > 0" class="d-flex flex-wrap">
+                        <div v-for="(image, index) in imagePreviews" :key="index">
+                            <img :src="image" alt="Anteprima immagine" class="me-2 date-image-preview" >
+                        </div>
+                    </div>
                 </div>
-                <div class="col-12 d-flex align-items-center justify-content-center">
-                    <button class="btn-show text-center" type="submit">Crea Spettacolo</button>
+                <div class="col-12 d-flex align-items-center justify-content-center animated animate__fadeInUp">
+                    <button class="btn-show border border-2 text-center" type="submit">Crea Spettacolo</button>
                 </div>
             </div>
         </form>
@@ -68,9 +77,65 @@ export default {
                 show_type_id: '',
                 pranzo_cena: 'pranzo',
                 data: data,
+                img: [],
                 prezzo : 0
+
             }
         );
+        const imagePreviews = ref([]);
+        // Aggiungo i file all array, creao un anteprima e ridimensiono i file per l'upload (meno byte piu velocità )
+        function onFileChange(event) {
+            const files = event.target.files;
+
+            for (const key in files) {
+                if (files.hasOwnProperty(key)) {
+                const originalFile = files[key];
+
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    const dataURL = e.target.result;
+
+                    const img = new Image();
+                    img.src = dataURL;
+
+                    img.onload = () => {
+                    const maxWidth = 200; // Larghezza massima desiderata
+                    const maxHeight = 200; // Altezza massima desiderata
+
+                    let newWidth, newHeight;
+                    if (img.width > img.height) {
+                        newWidth = maxWidth;
+                        newHeight = (img.height / img.width) * maxWidth;
+                    } else {
+                        newHeight = maxHeight;
+                        newWidth = (img.width / img.height) * maxHeight;
+                    }
+
+                    // Crea un canvas per ridimensionare l'immagine
+                    const canvas = document.createElement('canvas');
+                    const ctx = canvas.getContext('2d');
+                    canvas.width = newWidth;
+                    canvas.height = newHeight;
+                    ctx.drawImage(img, 0, 0, newWidth, newHeight);
+
+                    // Converti il canvas in un nuovo oggetto Blob (File)
+                    canvas.toBlob((blob) => {
+                        const modifiedFile = new File([blob], originalFile.name, {
+                        type: originalFile.type,
+                        lastModified: originalFile.lastModified,
+                        size: blob.size,
+                        });
+
+                        formData.value.img.push(modifiedFile);
+                        imagePreviews.value.push(URL.createObjectURL(modifiedFile));
+                    }, originalFile.type);
+                    };
+                };
+                reader.readAsDataURL(originalFile);
+                }
+            }
+            console.log(formData.value.img)
+        }
 
         const createReservationDate = async () => {
             try {
@@ -131,12 +196,25 @@ export default {
             createReservationDate,
             showTypes,
             onPriceBlur,
-            formatCurrency
+            formatCurrency,
+            onFileChange,
+            imagePreviews
         }
     },
 };
 </script>
 
-<style>
-
+<style scoped>
+    .date-image-preview{
+        max-height: 150px;
+        max-width: 150px;
+        object-fit: contain;
+        object-position: center;
+        cursor: pointer;
+    }
+    .date-image-preview:hover{
+        border-radius: 10px;
+        transform: scale(1.2);
+        transition: all .3s;
+    }
 </style>
